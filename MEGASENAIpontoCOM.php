9ae72d3fca8fdadcda9594072f6ea48b4b801bd4
Gabriel Fonseca <gabrielramooos08@gmail.com>
2025-11-26 11:13:07 -0300

    Refactor menu and game functions for clarity

--- MEGASENAIpontoCOM.php
+++ MEGASENAIpontoCOM.php
@@ -1,12 +1,12 @@
 <?php
 
-    menu();
+menu();
 
-function menu(): void{
+function menu(): void {
 
     $continue = true;
 
-    do{
+    do {
 
         print("|           *BIG-SHOT GAMBLING*         |\n");
         print("=========================================\n");
@@ -19,7 +19,7 @@ function menu(): void{
 
         $opcao = trim(readline(""));
 
-        switch($opcao){
+        switch ($opcao) {
             case '1':
                 print("JOGO ESCOLHIDO: Mega Sena.\n");
                 MegaSena();
@@ -37,167 +37,203 @@ function menu(): void{
                 Lotofacil();
                 break;
             case '0':
-                print("saindo...\n");
+                print("Saindo...\n");
+                $continue = false; 
                 break;
-                $continue == false;
-                
-            
-            default :
-                print("Opção invalida. \n");
+            default:
+                print("Opção inválida.\n");
                 break;
-            
         }
-    } while ($continue == true);
 
+    } while ($continue);
 }
 
-function MegaSena(){
+
+
+function MegaSena() {
 
     print("|          *MEGA-SENA*          |\n");
     print("=================================\n");
 
+    $qntdJgs = readline("|QUANTIDADE DE JOGOS: ");
+    $qntdDezenas = readline("|QUANTIDADE DE NÚMEROS (6 a 20): ");
 
-
-    $sort = [];
-
-    $qntdJgs = readline("|QUANTIDADE DE JOGOS:\n");
-    $qntdDezenas = readline("|QUANTIDADE DE NUMEROS:\n");
-
-    for ($i=0; $i < $qntdJgs; $i++) { 
-        
-    while(count($sort) < $qntdDezenas){
-    if($qntdDezenas < 6 or $qntdDezenas > 20){
-        print("Quantidade invalida");
-        break;
-    }else{
-        $DezSort = rand(1, 60);
-        if (!in_array($DezSort, $sort)){
-            $sort[] = $DezSort;
-        }
-        }
-    }
     
-    
-    sort($sort);
-    
-    foreach ($sort as $valor) {
-        print "| $valor ";
+    if ($qntdDezenas < 6 || $qntdDezenas > 20) {
+        print("Quantidade inválida!\n");
+        return;
     }
 
-    $sort = [];
-    print("\n");
+    $valorAposta = calcularMega($qntdDezenas);
+
+    for ($i = 0; $i < $qntdJgs; $i++) {
+
+        $sort = [];
+
+        while (count($sort) < $qntdDezenas) {
+            $num = rand(1, 60);
+            if (!in_array($num, $sort)) {
+                $sort[] = $num;
+            }
+        }
+
+        sort($sort);
+
+        print("Aposta " . ($i + 1) . ": ");
+        foreach ($sort as $valor) {
+            print "$valor ";
+        }
+
+        print(" | VALOR: R$ " . number_format($valorAposta, 2, ',', '.') . "\n");
+    }
 }
 
+function calcularMega($qtd) {
+
+    $valores = [
+        6 => 5.00,
+        7 => 35.00,
+        8 => 140.00,
+        9 => 420.00,
+        10 => 1050.00,
+        11 => 2310.00,
+        12 => 4620.00,
+        13 => 8580.00,
+        14 => 15015.00,
+        15 => 25025.00,
+        16 => 40040.00,
+        17 => 61880.00,
+        18 => 92820.00,
+        19 => 135660.00,
+        20 => 193800.00,
+    ];
+
+    return $valores[$qtd];
 }
 
-function Quina(){
-   
+
+function Quina() {
+
     print("|          *QUINA*         |\n");
     print("============================\n");
 
+    $qntdJgs = readline("|QUANTIDADE DE JOGOS: ");
+    $qntdDezenas = readline("|QUANTIDADE DE NÚMEROS (5 a 15): ");
 
-
-    $sort = [];
-
-    $qntdJgs = readline("|QUANTIDADE DE JOGOS:\n");
-    $qntdDezenas = readline("|QUANTIDADE DE NUMEROS:\n");
-
-    for ($i=0; $i < $qntdJgs; $i++) { 
-        
-    while(count($sort) < $qntdDezenas){
-    if($qntdDezenas < 5 or $qntdDezenas > 15){
-        print("Quantidade invalida");
-        break;
-    }else{
-        $DezSort = rand(1, 80);
-        if (!in_array($DezSort, $sort)){
-            $sort[] = $DezSort;
-        }
-        }
-    }
-    
-    
-    sort($sort);
-    
-    foreach ($sort as $valor) {
-        print "| $valor ";
+    if ($qntdDezenas < 5 || $qntdDezenas > 15) {
+        print("Quantidade inválida!\n");
+        return;
     }
 
-    $sort = [];
-    print("\n");
-} 
+    $valorAposta = calcularQuina($qntdDezenas);
+
+    for ($i = 0; $i < $qntdJgs; $i++) {
+
+        $sort = [];
+
+        while (count($sort) < $qntdDezenas) {
+            $num = rand(1, 80);
+            if (!in_array($num, $sort)) {
+                $sort[] = $num;
+            }
+        }
+
+        sort($sort);
+
+        print("Aposta " . ($i + 1) . ": ");
+        foreach ($sort as $valor) {
+            print "$valor ";
+        }
+
+        print(" | VALOR: R$ " . number_format($valorAposta, 2, ',', '.') . "\n");
+    }
 }
 
-function Lotomania(){
-    
+function calcularQuina($qtd) {
+   
+    $base = 2.50;
+    return $base * ($qtd - 4); 
+}
+
+
+function Lotomania() {
+
     print("|          *LOTOMANIA*         |\n");
     print("================================\n");
 
+    $qntdJgs = readline("|QUANTIDADE DE JOGOS: ");
 
+    $valorAposta = 3.00;
 
-    $sort = [];
+    for ($i = 0; $i < $qntdJgs; $i++) {
 
-    $qntdJgs = readline("|QUANTIDADE DE JOGOS:\n");
-    $qntdDezenas = readline("|DIGITE 50 PARA GERAR:\n");
+        $sort = [];
 
-    for ($i=0; $i < $qntdJgs; $i++) { 
-        
-    while(count($sort) < $qntdDezenas){
-    if($qntdDezenas != 50){
-        print("Quantidade invalida");
-        break;
-    }else{
-        $DezSort = rand(1, 100);
-        if (!in_array($DezSort, $sort)){
-            $sort[] = $DezSort;
+        while (count($sort) < 50) {
+            $num = rand(0, 99);
+            if (!in_array($num, $sort)) {
+                $sort[] = $num;
+            }
         }
-        }
-    }
-    
-    
-    sort($sort);
-    
-    foreach ($sort as $valor) {
-        print "| $valor ";
-    }
 
-    $sort = [];
-    print("\n");
-}
+        sort($sort);
+
+        print("Aposta " . ($i + 1) . ": ");
+        foreach ($sort as $valor) {
+            print "$valor ";
+        }
+
+        print(" | VALOR: R$ " . number_format($valorAposta, 2, ',', '.') . "\n");
+    }
 }
 
-function Lotofacil(){
-   
+
+function Lotofacil() {
+
     print("|          *LOTOFACIL*         |\n");
     print("================================\n");
 
-    $sort = [];
+    $qntdJgs = readline("|QUANTIDADE DE JOGOS: ");
+    $qntdDezenas = readline("|QUANTIDADE DE NÚMEROS (15 a 20): ");
 
-    $qntdJgs = readline("|QUANTIDADE DE JOGOS:\n");
-    $qntdDezenas = readline("|QUANTIDADE DE NUMEROS:\n");
-
-    for ($i=0; $i < $qntdJgs; $i++) { 
-        
-    while(count($sort) < $qntdDezenas){
-    if($qntdDezenas < 15 or $qntdDezenas > 20){
-        print("Quantidade invalida");
-        break;
-    }else{
-        $DezSort = rand(1, 25);
-        if (!in_array($DezSort, $sort)){
-            $sort[] = $DezSort;
-        }
-        }
-    }
-    
-    
-    sort($sort);
-    
-    foreach ($sort as $valor) {
-        print "| $valor ";
+    if ($qntdDezenas < 15 || $qntdDezenas > 20) {
+        print("Quantidade inválida!\n");
+        return;
     }
 
-    $sort = [];
-    print("\n");
-}  
+    $valorAposta = calcularLotofacil($qntdDezenas);
+
+    for ($i = 0; $i < $qntdJgs; $i++) {
+
+        $sort = [];
+
+        while (count($sort) < $qntdDezenas) {
+            $num = rand(1, 25);
+            if (!in_array($num, $sort)) {
+                $sort[] = $num;
+            }
+        }
+
+        sort($sort);
+
+        print("Aposta " . ($i + 1) . ": ");
+        foreach ($sort as $valor) {
+            print "$valor ";
+        }
+
+        print(" | VALOR: R$ " . number_format($valorAposta, 2, ',', '.') . "\n");
+    }
+}
+
+function calcularLotofacil($qtd) {
+    $valores = [
+        15 => 3.00,
+        16 => 48.00,
+        17 => 408.00,
+        18 => 2440.00,
+        19 => 11628.00,
+        20 => 46512.00,
+    ];
+
+    return $valores[$qtd];
 }
